a56a4b537267b01e352ab6f5b63b62a7abfa293b
Nishchitha Nagaraj <93251870+NishchithaN1@users.noreply.github.com>
2024-04-02 10:13:15 +0530

    Time: 20 ms (26.87%), Space: 45.2 MB (21.27%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        HashSet<List<Integer>> set=new HashSet<>();
+        backtrack(res,nums,0,set);
+
+        return res;        
+    }
+
+    public void backtrack(List<List<Integer>> list,int[] nums ,int index,HashSet<List<Integer>> set){
+        if(index==nums.length){
+            List<Integer> current=toList(nums);
+            if(!set.contains(current)){
+                list.add(current);
+                set.add(current);
+            }
+        }else{
+            for(int i=index;i<nums.length;i++){
+                swap(index,i,nums);
+                backtrack(list,nums,index+1,set);
+                swap(index,i,nums);
+            }
+        }
+
+      
+    }
+
+    public void swap(int m, int n,int[] nums){
+            int temp=nums[m];
+            nums[m]=nums[n];
+            nums[n]=temp;
+    }
+
+    public List<Integer> toList(int[] nums){
+            ArrayList<Integer> l=new ArrayList<>();
+            for(int n:nums){
+                l.add(n);
+            }
+        return l;
+    }       
+}
